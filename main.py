800a6272ff1a96182f83b9e1b839c3f3db571c1b
Trenisha Goslee <trenishagoslee@ADA-Trenisha.local>
2021-05-11 17:22:57 -0400

    adding two functions to main.py

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+def rot13(letter):
+    if 'A' <= letter <= 'Z':
+        offset = ord('A')
+    elif 'a' <= letter <= 'z':
+        offset = ord('a')
+    else:
+        return letter
+
+    offset_letter = ord(letter) - offset + 13
+    wrapped_letter = offset_letter % 26
+    new_letter = chr(wrapped_letter + offset)
+
+    return new_letter
+
+def caesar(msg):
+    return "".join([rot13(letter) for letter in msg])
+    
 def main():
     pass
     # print(f"{caesar('Hello')=}")
