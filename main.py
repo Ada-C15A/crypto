3b328bb0b631eca13cf486fc084fa8a8e69d99ca
Ansel Rognlie <36503186+anselrognlie@users.noreply.github.com>
2021-05-11 14:29:31 -0700

    adds functions for xor-based crypto

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+def crypt_letter(letter, key_letter):
+    return chr(ord(letter) ^ ord(key_letter))
+
+def crypt(msg, key):
+    key_len = len(key)
+    return "".join([crypt_letter(letter, key[i % key_len]) for i, letter in enumerate(msg)])
+
 def main():
     pass
     # print(f"{caesar('Hello')=}")
